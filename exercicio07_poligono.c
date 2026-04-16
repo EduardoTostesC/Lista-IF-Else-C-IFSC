1790325e26ddef551f0886cfe21bf49bfbf4e0c4
Adilson Eduardo Tostes Carmo <eduveltrini@gmail.com>
2026-04-16 15:05:27 -0300

    Create exercicio07_poligono.c
    Fazendo o exercicio 7

---
+++ exercicio07_poligono.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int lados;
+    float medida, area;
+
+    printf("3 - TRIANGULO\n");
+    printf("4 - QUADRADO\n");
+    printf("5 - PENTAGONO\n");
+    printf("Insira o numero de lados do poligono:");
+    scanf("%d", &lados);
+
+    printf("\nInsira a medida do lado em cm: ");
+    scanf("%f", &medida);
+
+    switch (lados) {
+        case 3:
+        printf("TRIANGULO\n");
+        area = (medida * medida * 1.732) / 4;
+        printf("Area: %.2f\n", area);
+        break;
+
+        case 4:
+        printf("QUADRADO\n");
+        area = medida * medida;
+        printf("Area: %.2f\n", area);
+        break;
+
+        case 5:
+        printf("PENTAGONO\n");
+        break;
+            
+        default:
+        printf("Número de lados de poligono incalculavel neste programa\n");
+        break;
+        
+    }
+
+    return 0;
+}
